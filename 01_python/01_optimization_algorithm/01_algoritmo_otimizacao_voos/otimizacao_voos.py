6a86ba62ebc2607a4a773cc98f5fdd259e0342be
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-03-23 17:26:12 -0300

    Editing otimizacao_voos.py file

--- 01_python/01_optimization_algorithm/01_algoritmo_otimizacao_voos/otimizacao_voos.py
+++ 01_python/01_optimization_algorithm/01_algoritmo_otimizacao_voos/otimizacao_voos.py
@@ -16,11 +16,13 @@ destino = "GRU"
 
 voos = {}
 
-with open("voos.txt", "w+") as arquivo:
+with open("01_algoritmo_otimizacao_voos/voos.txt", "r") as arquivo:
     for linha in arquivo:
         origem, destino, saida, chegada, preco = linha.split(",")
         voos.setdefault((origem, destino), [])
         voos[(origem, destino)].append((saida, chegada, int(preco)))
+
+# print(voos)
 
 # [1,4 3,2 7,3 6,3 2,4 5,3]
 
@@ -33,7 +35,7 @@ def imprimir_agenda(agenda):
         ida = voos[(origem, destino)][agenda[id_voo]]
         id_voo += 1
         volta = voos[(destino, origem)][agenda[id_voo]]
-        print("%10s%10s %5s-%5s R$%3s  %5s-%5s R$%3s" % (nome, origem, ida[0], ida[1], ida[2]))
+        print(f"{nome}, {origem}, {ida[0]}, {ida[1]}, R$ {ida[2]}")
 
 agenda = [1,4, 3,2, 7,3, 6,3, 2,4, 5,3]
 imprimir_agenda(agenda)
